84584ab2af13dd63dd3acd49c733a97fcf4e01a6
Matthew Elliott <matthew@seoworks.co.uk>
2024-12-09 16:10:57 +0000

    addded the dice roll functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const diceRoll = document.querySelector('.btn--roll');
+const holdButton = document.querySelector('.btn--hold');
+const diceImage = document.querySelector('.dice');
+const player0CurrentScore = document.querySelector('#current--0');
+const player1CurrentScore = document.querySelector('#current--1');
+
+diceRoll.addEventListener('click', function () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+  diceImage.src = `dice-${randomNumber}.png`;
+  console.log(randomNumber);
+  if (randomNumber !== 1) {
+    player0CurrentScore.textContent =
+      Number(player0CurrentScore.textContent) + randomNumber;
+  }
+});
